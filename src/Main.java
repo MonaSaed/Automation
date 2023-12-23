7e48faf2b71112de0abb1a70cc2a535fb55bb6b4
Your Name <you@example.com>
2023-12-23 15:30:05 +0200

    changed world to Ibrahim

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ahmed" +
-                "");
+        System.out.println("Hello Ibrahim");
+
     }
-}z
+}           
